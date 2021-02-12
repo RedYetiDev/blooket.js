9e7acdd356da7cafd306e8c4fc727a27256523f8
RedYetiev <38299977+RedYetiDev@users.noreply.github.com>
2021-02-12 07:37:22 -0500

    Add more content

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 gsap.registerPlugin(ScrollTrigger)
 window.onload = function() {
   document.getElementsByClassName("setup")[0].style.opacity = '0'
-  gsap.to(".menu", {
+  document.getElementsByClassName("documentation")[0].style.opacity = '0'
+  gsap.to(".first", {
     scrollTrigger: {
     trigger: ".header :not(.setup)",
     start: "top top",
@@ -18,13 +19,15 @@ window.onload = function() {
   },
   opacity: 1
   });
-  ScrollTrigger.create({
-    trigger: ".setup div.header",
-    start: "top top",
-    endTrigger: ".setup-end",
-    end: "bottom top",
-    pin: true,
-    duration: 0.3,
-    ease: "power1.inOut"
-  })
+  gsap.to(".documentation", {
+    scrollTrigger: {
+    trigger: ".documentation",
+    start: "top bottom-=100",
+    endTrigger: ".documentation-header",
+    start: "top bottom-=100",
+    scrub: true,
+    markers: true
+  },
+  opacity: 1
+  });
 }

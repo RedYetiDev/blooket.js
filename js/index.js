160b8c102feeb34fba4515c27c39a1bed72070a6
RedYetiev <38299977+RedYetiDev@users.noreply.github.com>
2021-02-12 10:35:45 -0500

    Add more

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 gsap.registerPlugin(ScrollTrigger)
 window.onload = function() {
+  HTMLCollection.prototype.forEach = Array.prototype.forEach
+  NodeList.prototype.forEach = Array.prototype.forEach
   document.getElementsByClassName("setup")[0].style.opacity = '0'
-  document.getElementsByClassName("documentation")[0].style.opacity = '0'
+  document.getElementById("bi").style.opacity = '0'
+  document.getElementById("installation").style.opacity = '0'
+  document.getElementById("doc-setup").style.opacity = '0'
   gsap.to(".first", {
     scrollTrigger: {
     trigger: ".header :not(.setup)",
@@ -19,15 +23,39 @@ window.onload = function() {
   },
   opacity: 1
   });
-  gsap.to(".documentation", {
+  gsap.to(".bi", {
     scrollTrigger: {
-    trigger: ".documentation",
+    trigger: ".bi",
     start: "top bottom-=100",
-    endTrigger: ".documentation-header",
-    start: "top bottom-=100",
+    end: "top top",
     scrub: true,
     markers: true
   },
   opacity: 1
   });
+  gsap.to(".inst", {
+    scrollTrigger: {
+    trigger: ".inst",
+    start: "top bottom-=100",
+    end: "top top",
+    scrub: true,
+    markers: true
+  },
+  opacity: 1
+  });
+  gsap.to("#doc-setup", {
+    scrollTrigger: {
+    trigger: "#doc-setup",
+    start: "top bottom-=100",
+    end: "top top",
+    scrub: true,
+    markers: true
+  },
+  opacity: 1
+  });
+}
+function dark() {
+    document.body.classList.toggle('darkmode');
+    document.getElementsByClassName("header").forEach(function(e) {e.classList.toggle('darkmode')})
+    document.querySelectorAll('a').forEach(function(e) {e.classList.toggle('darkmode')})
 }
